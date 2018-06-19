a9f458b000072fa7853f8ee80a5108a8a9a0a2c6
Jose Elias Miguel Martinez Sedano <eliasm@mx1.ibm.com>
2018-06-19 00:33:35 -0500

    add images for doc ilustartion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import Moment from 'moment';
 
 class App extends Component {
   render() {
-    const date = Moment().add(5, 'seconds');
+    const date = Moment().add(2, 'days');
+    /**
+     * Use one of for test different dates: 
+     const date = Moment().add(2, 'days'); // <-- image date seems to be 2 days ahead
+     const date = Moment().add(5, 'seconds'); // <-- use this for fun
+     const date = Moment().add(1, 'minutes').add(5, 'seconds');
+     const date = Moment('2018-07-01');
+     */
     return (
       <div className="App">
           <Countdown targetDate={date}/>
